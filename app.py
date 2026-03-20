661711a154c0203f69a60844b671129023795ea0
Leti1983 <marialeticia.lombardi@gmail.com>
2026-03-19 21:04:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ Tus reglas:
 
 # 4. Configuración del Modelo de IA
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-1.5-pro",
     system_instruction=frankl_prompt
 )
 
